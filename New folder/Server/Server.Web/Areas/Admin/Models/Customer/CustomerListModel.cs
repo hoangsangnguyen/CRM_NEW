ee57066942315e21530e712adfd32f0b2af28a0f
hoangsangnguyen <nguyenhoangsang6014@gmail.com>
2018-06-22 23:02:01 +0700

    update

---
+++ New folder/Server/Server.Web/Areas/Admin/Models/Customer/CustomerListModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vino.Server.Web.Areas.Admin.Models.Customer
+{
+    public class CustomerListModel
+    {
+    }
+}
