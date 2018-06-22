00ea2696a63c2fa15b0740d4fb0975120c191f14
hoangsangnguyen <nguyenhoangsang6014@gmail.com>
2018-06-23 06:08:46 +0700

    update

---
+++ New folder/Server/Server.Web/Areas/Admin/Models/Contact/ContactListModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vino.Server.Web.Areas.Admin.Models.Contact
+{
+    public class ContactListModel
+    {
+
+    }
+}
