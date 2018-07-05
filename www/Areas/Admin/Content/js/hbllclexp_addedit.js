134b2b835e151bba750f528701156fbf61c49734
hoangsangnguyen <nguyenhoangsang6014@gmail.com>
2018-07-05 07:07:46 +0700

    publish

--- www/Areas/Admin/Content/js/hbllclexp_addedit.js
+++ www/Areas/Admin/Content/js/hbllclexp_addedit.js
@@ -1,47 +1,53 @@
 ﻿$("#create-shipper").click(function () {
     const url = $(this).data('request-url');
-    OpenModalCreation(url, "ShipperId", "DrpCustomer");
+    OpenModalCreation(url, "ShipperId", ["ShipperId","ConsigneeId","NotifyPartyId"]);
 });
 
 $("#create-consignee").click(function () {
     const url = $(this).data('request-url');
-    OpenModalCreation(url, "ConsigneeId", "DrpCustomer");
+    OpenModalCreation(url, "ConsigneeId", ["ShipperId", "ConsigneeId", "NotifyPartyId"]);
 });
 
 $("#create-NotifyParty").click(function () {
     const url = $(this).data('request-url');
-    OpenModalCreation(url, "NotifyPartyId", "DrpCustomer");
+    OpenModalCreation(url, "NotifyPartyId", ["ShipperId", "ConsigneeId", "NotifyPartyId"]);
 });
 
 $("#create-FinalDestination").click(function () {
     const url = $(this).data('request-url');
-    OpenModalCreation(url, "FinalDestinationId", "DrpPort");
+    OpenModalCreation(url, "FinalDestinationId", ["FinalDestinationId", "PlaceOfDeliveryId",
+        "PortOfDischargeId", "PlaceOfReceiptId", "PortOfLoaingId", "TranshipmentPortId"]);
 });
 
 $("#create-PlaceOfDelivery").click(function () {
     const url = $(this).data('request-url');
-    OpenModalCreation(url, "PlaceOfDeliveryId", "DrpPort");
+    OpenModalCreation(url, "PlaceOfDeliveryId", ["FinalDestinationId", "PlaceOfDeliveryId",
+        "PortOfDischargeId", "PlaceOfReceiptId", "PortOfLoaingId", "TranshipmentPortId"]);
 });
 
 $("#create-PortOfDischarge").click(function () {
     const url = $(this).data('request-url');
-    OpenModalCreation(url, "PortOfDischargeId", "DrpPort");
+    OpenModalCreation(url, "PortOfDischargeId", ["FinalDestinationId", "PlaceOfDeliveryId",
+        "PortOfDischargeId", "PlaceOfReceiptId", "PortOfLoaingId", "TranshipmentPortId"]);
 });
 
 $("#create-PlaceOfReceipt").click(function () {
     const url = $(this).data('request-url');
-    OpenModalCreation(url, "PlaceOfReceiptId", "DrpPort");
+    OpenModalCreation(url, "PlaceOfReceiptId", ["FinalDestinationId", "PlaceOfDeliveryId",
+        "PortOfDischargeId", "PlaceOfReceiptId", "PortOfLoaingId", "TranshipmentPortId"]);
 });
 
 
 $("#create-PortOfLoaing").click(function () {
     const url = $(this).data('request-url');
-    OpenModalCreation(url, "PortOfLoaingId", "DrpPort");
+    OpenModalCreation(url, "PortOfLoaingId", ["FinalDestinationId", "PlaceOfDeliveryId",
+        "PortOfDischargeId", "PlaceOfReceiptId", "PortOfLoaingId", "TranshipmentPortId"]);
 });
 
 $("#create-TranshipmentPort").click(function () {
     const url = $(this).data('request-url');
-    OpenModalCreation(url, "TranshipmentPortId", "DrpPort");
+    OpenModalCreation(url, "TranshipmentPortId", ["FinalDestinationId", "PlaceOfDeliveryId",
+        "PortOfDischargeId", "PlaceOfReceiptId", "PortOfLoaingId", "TranshipmentPortId"]);
 });
 function OpenModalCreation(url, viewId, viewGroupId) {
     $.ajax({
@@ -50,7 +56,7 @@ function OpenModalCreation(url, viewId, viewGroupId) {
         cache: false,
         async: true,
         contentType: 'application/json',
-        data: JSON.stringify({ viewId: viewId, viewGroupId: viewGroupId }),
+        data: JSON.stringify({ viewId: viewId, viewGroupId: viewGroupId, model: null }),
         success: function (dataJs) {
             $('#myModalContent').html(dataJs);
             $("#myModal").modal("show");
